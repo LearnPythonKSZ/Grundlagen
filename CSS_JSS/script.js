850c99a67dbc2177c9c856b79c472b8a85fd9de1
LearnPythonKSZ <33370042+LearnPythonKSZ@users.noreply.github.com>
2017-12-22 14:54:27 +0100

    Update script.js

--- CSS_JSS/script.js
+++ CSS_JSS/script.js
@@ -1,14 +1,41 @@
+var points=0;
+var pointgap = 0;
+var pointanswerfunction = 0;
+var pointsimagemap=0;
+var pointquestion=0;
+var pointsneeded=1;
+var differenz= pointsneeded-points;
+function number(){
+	document.getElementById("point").innerHTML= points;
+	if(pointsneeded-points>0){
+		differenz= pointsneeded-points;
+		document.getElementById("diff").innerHTML=differenz;
+	}
+	else{
+		document.getElementById("unlocked").innerHTML="Du hast das nächste Kapitel freigeschaltet";
+	}
+}
+function pointunlock() {
+	points = pointgap + pointanswerfunction + pointquestion + pointsimagemap;
+	differenz= pointsneeded-points;
+	number();
+	if (points >= pointsneeded){
+		document.getElementById("next").className="nextunlocked";
+		document.getElementById("next").value="Nächstes Kapitel";
+		document.getElementById("buttonhref").href="/IDE"
+	}
+}
 function Question(name){
 	var elements = document.getElementsByName(name);
 	var bool = elements[0].getAttribute( 'id' );
 	if (bool=="true"){
 		document.getElementById("answer1").className="answer";
 		document.getElementById("answer2").className="answer";
-		document.getElementById("answer3").className="answertrue";
-		document.getElementById("answer4").className="answer";
-		document.getElementById("next").className="nextunlocked";
-		document.getElementById("next").value="Nächstes Kapitel";
-		document.getElementById("buttonhref").href="/IDE";		
+		document.getElementById("answer3").className="answer";
+		document.getElementById("answer4").className="answertrue";
+		number();
+		pointquestion=1;
+		pointunlock();
 	}else{
 		switch(name){
 			case "button1":
@@ -25,4 +52,24 @@ function Question(name){
 				break;
 		}
 	}
+}
+function answerfunction() {
+    var answer=document.getElementById("answercheck").value;
+	if (answer=="2.0"||answer=="2,0"){
+		pointanswerfunction = 1;
+		pointunlock();
+
+	}
+}
+function gap() {
+	var answer=document.getElementById("codebox1").value;
+	var solution=document.getElementById("codebox1").name;
+	if (answer==solution){
+		pointgap = 1;
+		pointunlock();
+	}
+}
+function imagemapsolved(){
+	pointsimagemap=1;
+	pointunlock();
 }
